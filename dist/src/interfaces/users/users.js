304f0f99ca2eff47c1f6ffc8a9d361d5cfdc68ba
Evan Hameed <63654749+evanhameed99@users.noreply.github.com>
2022-05-06 17:39:27 +0300

    remove dist

--- dist/src/interfaces/users/users.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoID = void 0;
-const mongoose_1 = require("mongoose");
-class mongoID extends mongoose_1.Types.ObjectId {
-}
-exports.mongoID = mongoID;
